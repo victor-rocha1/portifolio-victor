e884c1ac6149b8fa393bd586996c7bdf1fb1604d
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2026-02-11 11:17:36 -0300

    feat - nova ordem das experiências

--- src/assets/sections/Experiences/Experiences.jsx
+++ src/assets/sections/Experiences/Experiences.jsx
@@ -12,20 +12,20 @@ function Experiences() {
                         <div className="card-content">
                             <img src={logo_prodemge} alt="logo_prodemge" />
                             <div className="text-content">
-                                <small className='span1'>Out 2024 - <span className='span2'>Nov 2025</span></small>
-                                <h5>Estagiário</h5>
-                                <ul>
-                                    <li>Uso de Metodologias Ágeis: Scrum e Kanban</li>
-                                    <li>Escrita de Histórias de usuário e organização estruturada no GitLab</li>
-                                    <li>Manutenção e evolução do <span className='span3'>Sistema de Envio de Correspondências de Trânsito</span>.
-                                    </li>
-                                </ul>
                                 <small className='span1'>Dez 2025 - <span className='span2'>atual</span></small>
                                 <h5>Assessor de T.I</h5>
                                 <ul>
                                     <li>Manutenção e evolução do <span className='span3'>Sistema do Protocolo Digital</span>.
                                     </li>
                                     <li>Uso de CakePhP, Oracle e Bootstrap.
+                                    </li>
+                                </ul>
+                                <small className='span1'>Out 2024 - <span className='span2'>Nov 2025</span></small>
+                                <h5>Estagiário</h5>
+                                <ul>
+                                    <li>Uso de Metodologias Ágeis: Scrum e Kanban</li>
+                                    <li>Escrita de Histórias de usuário e organização estruturada no GitLab</li>
+                                    <li>Manutenção e evolução do <span className='span3'>Sistema de Envio de Correspondências de Trânsito</span>.
                                     </li>
                                 </ul>
                             </div>
