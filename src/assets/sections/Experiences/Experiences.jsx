a2d19174eb43da23f822944870de8797c68b3bb4
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2026-05-07 20:41:03 -0300

    style: redesenha estilo da section experiences

--- src/assets/sections/Experiences/Experiences.jsx
+++ src/assets/sections/Experiences/Experiences.jsx
@@ -6,31 +6,45 @@ function Experiences() {
     return (
         <>
             <section className="experiências" id='experiences'>
-                <br /><br /><Title text="Minha " spantext="Experiência" /> <br />
-                <div className="interface">
-                    <div className="timeline">
-                        <div className="card-content">
-                            <img src={logo_prodemge} alt="logo_prodemge" />
-                            <div className="text-content">
-                                <small className='span1'>Dez 2025 - <span className='span2'>atual</span></small>
-                                <h5>Assessor de T.I</h5>
-                                <ul>
-                                    <li>Manutenção e evolução do <span className='span3'>Sistema do Protocolo Digital</span>.
-                                    </li>
-                                    <li>Uso de CakePhP, Oracle e Bootstrap.
-                                    </li>
-                                </ul>
-                                <small className='span1'>Out 2024 - <span className='span2'>Nov 2025</span></small>
-                                <h5>Estagiário</h5>
-                                <ul>
-                                    <li>Uso de Metodologias Ágeis: Scrum e Kanban</li>
-                                    <li>Escrita de Histórias de usuário e organização estruturada no GitLab</li>
-                                    <li>Manutenção e evolução do <span className='span3'>Sistema de Envio de Correspondências de Trânsito</span>.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                <br /><br /> <br />
+                <Title text="Minha " spantext="Experiência" />
 
+                <div className="timeline-container">
+                    {/* Esta é a linha que atravessa o centro */}
+                    <div className="timeline-line"></div>
+
+                    {/* Card 1: Assessor de T.I (Lado Direito) */}
+                    <div className="timeline-item right">
+                        <div className="timeline-dot"></div>
+
+                        <div className="timeline-card">
+                            <div className="card-header">
+                                <small className='span1'>Dez 2025 - <span className='span2'>atual</span></small>
+                                <img src={logo_prodemge} alt="Logo Prodemge" className='img-card-timeline' />
+                            </div>
+                            <h5>Assessor de T.I</h5>
+                            <ul>
+                                <li>Manutenção e evolução do <span className='span3'>Sistema do Protocolo Digital</span>.</li>
+                                <li>Uso de CakePhP, Oracle e Bootstrap.</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    {/* Card 2: Estagiário (Lado Esquerdo)  */}
+                    <div className="timeline-item left">
+                        <div className="timeline-dot"></div>
+                        <div className="timeline-card">
+                            <div className="card-header">
+                                <small className='span1'>Dez 2025 - <span className='span2'>atual</span></small>
+                                <img src={logo_prodemge} alt="Logo Prodemge" className='img-card-timeline' />
+                            </div>
+                            <h5>Estagiário</h5>
+                            <ul>
+                                <li>Uso de Metodologias Ágeis: Scrum e Kanban</li>
+                                <li>Escrita de Histórias de usuário e organização estruturada no GitLab</li>
+                                <li>Manutenção e evolução do <span className='span3'>Sistema de Envio de Correspondências de Trânsito</span>.</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </section>
